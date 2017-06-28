9f7a18ca710315c434446ff0e49b2fdf52e02ea5
amoyakd <yomauttad@gmail.com>
2017-06-28 18:41:00 +0530

    Update CodeSnippets.R

--- CodeSnippets.R
+++ CodeSnippets.R
@@ -1,4 +1,6 @@
-Histogram
-
+#Histogram
 hist(USstatePops$V1, breaks=20)
 
+#rnorm generates a random data set that fits the normal distribution
+#rnorm(no. of data points, mean, sd)
+rnorm(51, 6043834, 6823984)
