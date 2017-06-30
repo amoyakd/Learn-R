45f960f95edc3e0070776efb7dcdec3a6f32f01e
amoyakd <yomauttad@gmail.com>
2017-06-30 16:08:03 +0530

    Update CodeSnippets.R

--- CodeSnippets.R
+++ CodeSnippets.R
@@ -5,6 +5,10 @@ hist(USstatePops$V1, breaks=20)
 #rnorm(no. of data points, mean, sd)
 rnorm(51, 6043834, 6823984)
 
+#The rpois() function will generate a stream of random numbers that roughly fit the Poisson distribution. The fit gets better as you ask for a larger and larger sample.
+#The second number is the average delay between arrivals that you want the random number generator to try to come close to.
+rpois(no. of data points,average delay)
+
 #Drawing a sample
 sample(USstatePops$V1,size=16,replace=TRUE)
 
